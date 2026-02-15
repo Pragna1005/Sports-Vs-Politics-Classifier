24bafc9a6538432a4b353e415b9b3ac666eca083
Pragna Sree <pragnasree.muvva06@gmail.com>
2026-02-15 14:29:06 +0530

    added some comments

--- src/train.py
+++ src/train.py
@@ -7,43 +7,27 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score, classification_report
 
-
-# -------------------------
 # Load Dataset
-# -------------------------
 def load_data():
     df = pd.read_csv("/Users/pragnasree/Desktop/Github/Sports-Vs-Politics-Classifier/Data/df_file.csv")
-
-    # Keep only labels 0 and 1
-    df = df[df["Label"].isin([0, 1])]
-
-    # Convert labels to names
-    df["Label"] = df["Label"].map({0: "POLITICS", 1: "SPORTS"})
-
-    # Clean text
+    df = df[df["Label"].isin([0, 1])] # Keeping only labels 0 and 1
+    df["Label"] = df["Label"].map({0: "POLITICS", 1: "SPORTS"}) # Converting labels to names
     df["Text"] = df["Text"].apply(clean_text)
-
     return df
 
-
-# -------------------------
 # Text Cleaning
-# -------------------------
 def clean_text(text):
     text = text.lower()
     text = re.sub(r"[^a-z\s]", " ", text)
     return text
 
-
-# -------------------------
 # Evaluate Function
-# -------------------------
 def evaluate_models(X_train, X_test, y_train, y_test, title):
-    print(f"\n===== {title} =====")
+    print(f"\n----- {title} -----")
 
-    models = {
+    models = { # Machine learning models
         "Naive Bayes": MultinomialNB(),
-        "Logistic Regression": LogisticRegression(max_iter=1000),
+        "Logistic Regression": LogisticRegression(max_iter=1200),
         "SVM": LinearSVC()
     }
 
@@ -53,14 +37,9 @@ def evaluate_models(X_train, X_test, y_train, y_test, title):
         acc = accuracy_score(y_test, preds)
         print(f"{name} Accuracy: {acc:.4f}")
 
-
-# -------------------------
-# Main
-# -------------------------
 def main():
 
     df = load_data()
-
     X = df["Text"]
     y = df["Label"]
 
@@ -68,19 +47,21 @@ def main():
         X, y, test_size=0.2, random_state=42
     )
 
-    # 1️⃣ Bag of Words
+    # Feature representation
+
+    # Bag of Words
     bow = CountVectorizer()
     X_train_bow = bow.fit_transform(X_train)
     X_test_bow = bow.transform(X_test)
     evaluate_models(X_train_bow, X_test_bow, y_train, y_test, "Bag of Words")
 
-    # 2️⃣ TF-IDF
+    # TF-IDF
     tfidf = TfidfVectorizer()
     X_train_tfidf = tfidf.fit_transform(X_train)
     X_test_tfidf = tfidf.transform(X_test)
     evaluate_models(X_train_tfidf, X_test_tfidf, y_train, y_test, "TF-IDF")
 
-    # 3️⃣ TF-IDF + Bigrams
+    # TF-IDF + Bigrams
     tfidf_bigram = TfidfVectorizer(ngram_range=(1,2))
     X_train_bigram = tfidf_bigram.fit_transform(X_train)
     X_test_bigram = tfidf_bigram.transform(X_test)
